66e2ce5c4c981e52df60967637ba15f8a90c7465
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-16 18:38:55 +0200

    feat: add header with static links

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <script>
-import { router } from '../router';
+// import { router } from '../router';
 
 export default{
     name: "AppHeader",
@@ -13,11 +13,58 @@ export default{
 </script>
 
 <template>
-<div>
-    
-</div>
+<!-- nav bar -->
+    <nav class="navbar navbar-expand-lg bg-black p-4 sticky-top">
+
+        <div class="container-fluid">
+            <!-- logo -->
+            <a class="navbar-brand" href="#">
+                <img src="/assets/Logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+            </a>
+            
+            <!-- menu di navigazione -->
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <!-- sezione del dropdown -->
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>            
+        </div>
+    </nav>
+
+   
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+
+.navbar-nav .nav-link{
+    color: $secondary;
+}
 
 </style>
