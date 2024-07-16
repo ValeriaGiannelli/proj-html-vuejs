a772e93c5d9303365ca2cbf9cb59f5e67b97c651
RiccardoMacchi <rick.mac97@outlook.com>
2024-07-16 17:41:29 +0200

    feat: add general scss

--- src/components/CounterIcon.vue
+++ src/components/CounterIcon.vue
@@ -5,7 +5,19 @@ export default{
 </script>
 
 <template>
-
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <img src="../../public/assets/image (13).svg" alt="">
+            </div>
+            <div class="col">
+                <img src="../../public/assets/image (13).svg" alt="">
+            </div>
+            <div class="col">
+                <img src="../../public/assets/image (13).svg" alt="">
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
