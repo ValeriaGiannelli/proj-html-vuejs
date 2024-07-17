8294aaa9fbc627f386483aef04573f4c875ac106
Samuele <samueletasciotti@icloud.com>
2024-07-17 12:12:34 +0200

    feat: base of about us

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,24 @@ import AppHeader from './components/AppHeader.vue';
 import AppAbout from './pages/AppAbout.vue';
 import AppContact from './pages/AppContact.vue';
 import AppHome from './pages/AppHome.vue';
+import AppBread from './components/AppBread.vue';
+import DescriptionLeft from './components/DescriptionLeft.vue';
+import DescriptionRight from './components/DescriptionRight.vue';
+import AlbumNow from './components/AlbumNow.vue';
 
 
 export default{
   name: "App",
   components:{
-    AppAbout,
-    AppContact,
-    AppHeader,
-    AppHome,
-    AppFooter
+    AppAbout, //ABOUT
+    AppContact,//CONTACT US
+    AppHeader, // For all page
+    AppHome, //HOME PAGE
+    AppFooter, 
+    AppBread,
+    DescriptionLeft,
+    DescriptionRight,
+    AlbumNow
   }
 }
 </script>
@@ -23,7 +31,20 @@ export default{
 
   <AppHeader/>
   <main>
-    <AppHome />
+    <!-- <AppHome /> -->
+
+    <!-- DEBUG -->
+    <!-- <AppContact /> -->
+    <!-- <AppBread /> -->
+
+    <!-- Insserire App Paragrafo -->
+    <!-- <DescriptionLeft /> -->
+
+    <!-- Album out now -->
+    <!-- <AlbumNow /> -->
+
+     <!-- Insserire App Paragrafo -->
+    <!-- <DescriptionRight /> -->
   </main>
 
     <!-- <h1>Ciao sono app</h1> -->
