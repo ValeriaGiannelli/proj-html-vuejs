e00f4b8cc26e4906c6688772cdf63dbf47816f51
RiccardoMacchi <rick.mac97@outlook.com>
2024-07-17 12:19:08 +0200

    feat: add new video

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -32,29 +32,28 @@ export default{
             </div>
         </div>
     </section>
-<!-- Sezione tre icons -->
+<!-- Sezione tre icons e sense the jazz -->
     <div class="container-fluid" id="position_icons">
-        <div class="my_icons_home">
-            <div class="row">
-                <div class="col-3">
-                    <img src="../../public/assets/image (10).svg" alt="">
-                    <h3>ORIGINAL IDEAS</h3>
-                    <span>Contary Popular Belied, Lorem Ipsum Not Simply Ipsum Random Text.</span>
-                </div>
-                <div class="col-3">
-                    <img src="../../public/assets/image (11).svg" alt="">
-                    <h3>MUSIC STUDIO</h3>
-                    <span>Contary Popular Belied, Lorem Ipsum Not Simply Ipsum Random Text.</span>
-                </div>
-                <div class="col-3">
-                    <img src="../../public/assets/image (12).svg" alt="">
-                    <h3>ACOUSTIC COVERS</h3>
-                    <span>Contary Popular Belied, Lorem Ipsum Not Simply Ipsum Random Text.</span>
-                </div>
+        <div class="row my_icons_home">
+            <div class="col-3">
+                <img src="../../public/assets/image (10).svg" alt="">
+                <h3>ORIGINAL IDEAS</h3>
+                <span>Contary Popular Belied, Lorem Ipsum Not Simply Ipsum Random Text.</span>
+            </div>
+            <div class="col-3">
+                <img src="../../public/assets/image (11).svg" alt="">
+                <h3>MUSIC STUDIO</h3>
+                <span>Contary Popular Belied, Lorem Ipsum Not Simply Ipsum Random Text.</span>
+            </div>
+            <div class="col-3">
+                <img src="../../public/assets/image (12).svg" alt="">
+                <h3>ACOUSTIC COVERS</h3>
+                <span>Contary Popular Belied, Lorem Ipsum Not Simply Ipsum Random Text.</span>
             </div>
         </div>
-        <div class="row bg-black">
-            <div class="col">
+<!-- SENSE THE JEZZ -->
+        <div class="row bg-black justify-content-center">
+            <div class="col-9">
                 <h5>WHO WE ARE</h5>
                 <h2>SENSE THE JAZZ</h2>
                 <p>The Story Of Jazz And Blues Society Is The Story Of Determination, Passion, Idealism, Community, And Of Course, Music. Founded In 1985 And Incorporated In 1986, As A Community Based, Not-For-Profit, Charitable Arts Organization Located In Vancouver, British Columbia, The Society, From The Very Beginning, Has Worked Diligently To Establish Vancouver As A Centre For The Creation And Exchange Of Sounds And Ideas Between The Local, National And International Music Communities.</p>
@@ -62,6 +61,16 @@ export default{
             </div>      
         </div>
     </div>
+    <section>
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-9">
+                    <img src="../../public/assets/image (22).svg" alt="">
+                    <h2>NEW MUSIC VIDEO</h2>
+                </div>
+            </div>
+        </div>
+    </section>
        
     <CounterIcon/>
     <AlbumNow/>
@@ -99,26 +108,29 @@ export default{
 }
 #position_icons{
     position: relative;
+
+    h5{
+        margin-top: 100px;
+    }
 }
 
 .my_icons_home{
-    // position: absolute;
-    // transform: translate(0,-50%);
-    // top: 0;
-
-    .row{
-        justify-content: space-evenly;
-
-        .col-3{
-            background-color: #121212;
-            padding: 30px 0;
-            text-align: center;
-            img{
-                max-height: 150px;
-                filter: invert(1);
-            }
+    position: absolute;
+    transform: translate(-50%,-50%);
+    top: 0;
+    left: 50%;
+    justify-content: space-around;
+    width: 100%;
+    
+    .col-3{
+        background-color: #121212;
+        padding: 30px;
+        text-align: center;
+        img{
+            max-height: 150px;
+            filter: invert(1);
         }
-
     }
+
 }
 </style>
