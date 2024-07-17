120ea00a040fe563360e23bf348da549d13fb6f0
Samuele <samueletasciotti@icloud.com>
2024-07-17 11:43:49 +0200

    feat: store info

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 import { reactive } from 'vue'
 
 export const store = reactive({
-    navBar: [
-        {
-            name: "home"
-        }
-    ]
+    homeBooking:    {
+        email: 'demo@example.com',
+        cell: '1 123-456-789',
+        addres: '502 New Design Str, Melbourne, San Francisco, CA 94110, United States Of America​ Australia'  
+    }
+       
+
+
+
+
 });
